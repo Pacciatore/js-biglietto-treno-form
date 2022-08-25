68c722f89086bee8d8c8b35fd9b1786784ce1659
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-08-25 03:42:06 +0200

    interattività ticket-resume

--- js/script.js
+++ js/script.js
@@ -20,26 +20,43 @@ submitButton.addEventListener('click',
         const passengerAge = parseInt(document.getElementById('ageRange').value);
         console.log(passengerAge)
 
+        let ticketType = document.getElementById('ticket-type');
+        console.log('ticketType outside ' + ticketType.innerHTML);
+
         let ticketPrice = kmLength * costoPerKm;
-        console.log(ticketPrice);
+        console.log('ticketPrice outside ' + ticketPrice);
 
         switch (passengerAge) {
             case 0:
                 ticketPrice -= ticketPrice * scontoMinorenni;
-                console.log(ticketPrice);
+                ticketType.innerHTML = 'Biglietto Minori';
+                console.log('ticketType ' + ticketType.innerHTML)
+                console.log('ticketPrice ' + ticketPrice);
                 break;
 
             case 2:
                 ticketPrice -= ticketPrice * scontoSenior;
-                console.log(ticketPrice);
+                ticketType.innerHTML = 'Biglietto Senior';
+                console.log('ticketType ' + ticketType.innerHTML)
+                console.log('ticketPrice ' + ticketPrice);
                 break;
             default:
                 console.log('nessuno sconto');
                 break;
         }
 
-        const ticket = document.getElementById('biglietto');
+        // const ticket = document.getElementById('biglietto');
+        const passengerName = document.getElementById('passengerName').value;
+        const ticketName = document.getElementById('name');
+        const ticketFinalPrice = document.getElementById('ticket-price');
+        let wagon = document.getElementById('wagon');
+        const cpCode = document.getElementById('cp-code');
+
         console.log('Il costo del biglietto è: ' + ticketPrice.toFixed(2) + '€')
-        ticket.innerHTML = 'Il costo del biglietto è: ' + ticketPrice.toFixed(2) + '€';
+        ticketName.innerHTML = passengerName;
+        ticketFinalPrice.innerHTML = ticketPrice.toFixed(2) + '€';
+        wagon.innerHTML = Math.floor((Math.random() * 12) + 1);
+        cpCode.innerHTML = Math.floor(Math.random() * 90000) + 10000;
+        // ticket.innerHTML = 'Il costo del biglietto è: ' + ticketPrice.toFixed(2) + '€';
 
     })
